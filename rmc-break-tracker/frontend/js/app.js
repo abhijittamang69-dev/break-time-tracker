f812d27067cc2e10578fb83d6d99163c9dc61efa
abhijittamang69-dev <abhijittamang69@gmail.com>
2026-08-12 13:12:46 +0300

    Update API URL to Render deployment

--- rmc-break-tracker/frontend/js/app.js
+++ rmc-break-tracker/frontend/js/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://break-time-tracker.onrender.com/api';
 
 let currentUser = null;
 let authToken = localStorage.getItem('rmc_token');
